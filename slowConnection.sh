4a1a1745258862fc32d3163ef3e49670ddcb8ed7
Simon <sim.w80@gmail.com>
2013-03-11 11:45:54 +0100

    initial commit

---
+++ slowConnection.sh
@@ -0,0 +1,1 @@
+sudo ipfw pipe 1 config bw 30kbit/s plr 0.1 delay 1000ms; sudo ipfw add pipe 1 dst-port http
---
+++ verySlowConnection.sh
@@ -0,0 +1,1 @@
+sudo ipfw pipe 1 config bw 5kbit/s plr 0.3 delay 1000ms; sudo ipfw add pipe 1 dst-port http
